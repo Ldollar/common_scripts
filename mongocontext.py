480445d6f1ef8927848c1abf5f05cb89f31ed4ad
LSW <lsw@aituyou.com>
2017-11-16 18:37:00 +0800

    去除脏数据

--- mongocontext.py
+++ mongocontext.py
@@ -9,24 +9,20 @@ import pymongo
 
 class MongoDatabaseContext():
 
-    def __init__(self,host,user,password,database,port=27017):
+    def __init__(self, host, user, password, auth_database,database, port=27017):
         """初始化数据"""
-        # self.host = "101.37.68.53"
-        # self.port = 27017
-        # self.user = "vgedr"
-        # self.password = "vgedr123456"
-        # self.database = "vg-event-data"
         self.host = host
         self.port = port
         self.user = user
         self.password = password
+        self.auth_database = auth_database
         self.database = database
 
     def __enter__(self):
         """数据库连接，并返回cursor """
         self.client = pymongo.MongoClient(self.host, self.port)
         # 获得数据库的权限
-        self.client[self.database].authenticate(self.user, self.password, mechanism="SCRAM-SHA-1")
+        self.client[self.auth_database].authenticate(self.user, self.password, mechanism="SCRAM-SHA-1")
         control_database = self.client.get_database(name=self.database)
         return control_database
 
