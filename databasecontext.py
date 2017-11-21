8fe4c34866663bf667c7c1556aaeb4e77c6ba2fc
LSW <lsw@aituyou.com>
2017-11-21 17:11:31 +0800

    增加MySQL context manager

--- databasecontext.py
+++ databasecontext.py
@@ -3,6 +3,7 @@
 import json
 
 import pymongo
+import pymysql
 
 
 
@@ -25,6 +26,29 @@ class MongoDatabaseContext():
         self.client[self.auth_database].authenticate(self.user, self.password, mechanism="SCRAM-SHA-1")
         control_database = self.client.get_database(name=self.database)
         return control_database
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        """关闭数据库连接"""
+        self.client.close()
+        print "__exit__ zhixingla"
+        #return True
+
+class MysqlContext():
+    def __init__(self, host, user, password, auth_database,database, port=27017):
+        """初始化数据"""
+        self.host = host
+        self.port = port
+        self.user = user
+        self.password = password
+        self.auth_database = auth_database
+        self.database = database
+
+    def __enter__(self):
+        """数据库连接，并返回cursor """
+        self.db = pymysql.connect(host=self.host,user=self.user,password=self.password,db=self.auth_database,port=self.port)
+        # 获得数据库的权限
+        self.client = self.db.cursor()
+        return self.client
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         """关闭数据库连接"""
